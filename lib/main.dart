bc4d48cc6bfaad99b69d424e9d86e1edd2e3b8f7
Harsh Verma <vermaharsh463@gmail.com>
2023-06-30 00:17:50 +0530

    Firebase setup and Finished importing

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:safarnama/screens/splash_screen.dart';
+import 'screens/auth/login_screen.dart';
+import 'screens/package_overview_screen.dart';
+import 'screens/packages.dart';
+import 'screens/profile/refer_screen.dart';
+import 'screens/tab_pages/booking_screen.dart';
+import 'screens/tab_pages/favourite_screen.dart';
+import 'screens/tab_pages/home_screen.dart';
+import 'screens/tab_pages/profile_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+  ));
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -34,17 +49,17 @@ class MyApp extends StatelessWidget {
       ),
       initialRoute: SplashPage.routeNamed,
       routes: {
-        // LoginScreen.routeNamed: (BuildContext context) => LoginScreen(),
-        // SplashPage.routeNamed: (BuildContext context) => SplashPage(),
-        // HomeScreen.routeNamed: (BuildContext context) => HomeScreen(),
-        // PackageOverviewScreen.routeNamed: (BuildContext context) =>
-        //     PackageOverviewScreen(),
-        // // CheckoutScreen.routeNamed: (BuildContext context) => CheckoutScreen(),
-        // BookingScreen.routeNamed: (BuildContext context) => BookingScreen(),
-        // ProfileScreen.routeNamed: (BuildContext context) => ProfileScreen(),
-        // ReferScreen.routeNamed: (BuildContext context) => ReferScreen(),
-        // FavouriteScreen.routeNamed: (BuildContext context) => FavouriteScreen(),
-        // PackagesScreen.routeNamed: (BuildContext context) => PackagesScreen(),
+        LoginScreen.routeNamed: (BuildContext context) => LoginScreen(),
+        SplashPage.routeNamed: (BuildContext context) => SplashPage(),
+        HomeScreen.routeNamed: (BuildContext context) => HomeScreen(),
+        PackageOverviewScreen.routeNamed: (BuildContext context) =>
+            PackageOverviewScreen(),
+        // CheckoutScreen.routeNamed: (BuildContext context) => CheckoutScreen(),
+        BookingScreen.routeNamed: (BuildContext context) => BookingScreen(),
+        ProfileScreen.routeNamed: (BuildContext context) => ProfileScreen(),
+        ReferScreen.routeNamed: (BuildContext context) => ReferScreen(),
+        FavouriteScreen.routeNamed: (BuildContext context) => FavouriteScreen(),
+        PackagesScreen.routeNamed: (BuildContext context) => PackagesScreen(),
       },
     );
   }
